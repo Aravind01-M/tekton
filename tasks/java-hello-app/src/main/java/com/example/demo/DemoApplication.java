a368c3b5aee4e3364c8362bcebb63f741375b66b
Aravind <aravind.monapati@pronteff.com>
2025-08-28 14:46:02 +0530

    small change

--- tasks/java-hello-app/src/main/java/com/example/demo/DemoApplication.java
+++ tasks/java-hello-app/src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, OpenShift!";
+        return "Hello, OpenShift! && Congratulations <>..><";
     }
 }
 
